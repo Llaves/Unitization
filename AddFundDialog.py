92ae2e155861d8e9db36978b12d2c67564292043
Llaves <github@stripfamily.net>
2021-11-23 21:33:38 -0700

    AddFundDialog - generate warning if adding initial units to account with existing purchases

--- AddFundDialog.py
+++ AddFundDialog.py
@@ -6,9 +6,10 @@ Created on Sat Nov 20 15:42:13 2021
 """
 
 from add_fund import *
+from PyQt5.QtWidgets import QMessageBox
 
 class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
-  def __init__(self, parent = None):
+  def __init__(self, parent):
       QtWidgets.QDialog.__init__(self, parent)
       self.setupUi(self)
 
@@ -16,15 +17,41 @@ class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
       v = QtGui.QDoubleValidator()
       v.setBottom(0)
       v.setDecimals(3)
+      self.initialUnitsEdit.setText("0")
       self.initialUnitsEdit.setValidator(v)
+      if (len(parent.active_account.purchases) > 0):
+        self.initialUnitsEdit.editingFinished.connect(self.initialUnitsEntered)
       self.buttonBox.button(QtWidgets.QDialogButtonBox.Save).setEnabled(False)
       self.fundNameEdit.textChanged.connect(self.onTextChanged)
+      # although we initialize to 0, user could change this to blank, which would be invalid, but still
+      # pass validator
       self.initialUnitsEdit.textChanged.connect(self.onTextChanged)
+
+
 
   @QtCore.pyqtSlot()
   def onTextChanged(self):
     self.buttonBox.button(QtWidgets.QDialogButtonBox.Save).setEnabled(bool(self.initialUnitsEdit.text())
                                                                       and bool(self.fundNameEdit.text()))
+
+  # initial units should almost never be entered if there are already purchases in the account, as this will
+  # change all the unit counts that had been previously calculated. Require checking through a warning before
+  # allowing this
+  def initialUnitsEntered(self):
+    if (float(self.initialUnitsEdit.text()) > 0):
+      msg_box = QMessageBox()
+      msg_box.setText("You have entered an initial units value in an account that has existing fund purchases")
+      msg_box.setInformativeText("If you proceed, unit values for all funds in this account"\
+                                 "will change starting with the initial purchases.")
+      msg_box.setStandardButtons(QMessageBox.Save | QMessageBox.Discard)
+      msg_box.setDefaultButton(QMessageBox.Discard)
+      if (msg_box.exec() == QMessageBox.Discard):
+        self.initialUnitsEdit.setText("0")
+
+
+
+
+
 
 
   def fundName(self):
