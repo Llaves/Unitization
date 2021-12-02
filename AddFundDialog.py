a896e6cc9d2cc4090ef7bdf7bc2d2af33405a77f
Llaves <github@stripfamily.net>
2021-12-02 13:32:22 -0700

    fix problem with checking change to initial units

--- AddFundDialog.py
+++ AddFundDialog.py
@@ -24,8 +24,6 @@ class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
       v.setDecimals(3)
       self.initial_units.setText("0")
       self.initial_units.setValidator(v)
-      if (len(parent.active_account.purchases) > 0):
-        self.initial_units.editingFinished.connect(self.initialUnitsEntered)
       self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
       self.fund_name.textChanged.connect(self.onTextChanged)
       # although we initialize to 0, user could change this to blank, which would be invalid, but still
@@ -33,6 +31,8 @@ class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
       self.initial_units.textChanged.connect(self.onTextChanged)
       self.delete_fund.hide()
       self.delete_fund.stateChanged.connect(self.onCheckChange)
+      self.buttonBox.accepted.connect(self.initialUnitsCheck)
+      self.buttonBox.accepted.disconnect(self.accept)
 
       if self.edit_mode:
         self.fund_name.setText(self.old_fund.name)
@@ -51,9 +51,10 @@ class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
   # initial units should almost never be entered if there are already purchases in the account, as this will
   # change all the unit counts that had been previously calculated. Require checking through a warning before
   # allowing this
-  def initialUnitsEntered(self):
-    if ((((not self.edit_mode) and (float(self.initial_units.text()) > 0)) or
-      (self.edit_mode and (float(self.initial_units.text() != self.old_fund.initial_units))))
+  def initialUnitsCheck(self):
+    if ((((not self.edit_mode) and (float(self.initial_units.text()) > 0)
+          and (len(self.parent.active_account.purchases) > 0)) or
+         (self.edit_mode and (float(self.initial_units.text()) != self.old_fund.initial_units)))
         and self.parent.warnings_enabled):
       msg_box = QMessageBox()
       if self.edit_mode:
@@ -61,12 +62,20 @@ class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
       else:
         msg_box.setText("You have entered an initial units value in an account that has existing fund purchases")
       msg_box.setInformativeText("If you proceed, unit values for all funds in this account "\
-                                 "will change starting with the initial purchases.")
+                                  "will change starting with the initial purchases.")
       msg_box.setStandardButtons(QMessageBox.Save | QMessageBox.Discard)
       msg_box.setDefaultButton(QMessageBox.Discard)
       msg_box.setWindowTitle("UnitTracker Warning")
       if (msg_box.exec() == QMessageBox.Discard):
         self.initial_units.setText("0")
+        return
+
+    self.fund = Fund(0, self.fundName(), self.initialUnits(), self.parent.active_account.id)
+    if self.edit_mode:
+      self.fund.id = self.old_fund.id
+    QtWidgets.QDialog.accept(self)
+
+
 
   def onCheckChange(self):
     if self.delete_fund.isChecked():
@@ -87,9 +96,3 @@ class AddFundDialog(QtWidgets.QDialog, Ui_addFund):
 
   def initialUnitsChanged(self):
     return self.old_fund.initial_units != self.initialUnits()
-
-  def accept(self):
-    self.fund = Fund(0, self.fundName(), self.initialUnits(), self.parent.active_account.id)
-    if self.edit_mode:
-      self.fund.id = self.old_fund.id
-    QtWidgets.QDialog.accept(self)
