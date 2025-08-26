1d6cfcc5fa114f5b18bcacf68c250138d8e8b492
Llaves <GitHub@stripfamily.net>
2025-08-26 17:04:27 -0600

    modify pyinstaller script to copy empty.db to demo.db and main.db

--- compileDistribution.bat
+++ compileDistribution.bat
@@ -1,8 +1,8 @@
 call .venv\scripts\activate.bat
 rmdir /s /q dist build
 pyinstaller --onedir unitTracker.py 
-copy accounts.db dist\unitTracker\accounts.db
-copy demo.db dist\unitTracker\demo.db
+copy empty.db dist\unitTracker\accounts.db
+copy empty.db dist\unitTracker\demo.db
 copy icon.ico dist\unitTracker\icon.ico
 echo %%~dp0\unitTracker.exe accounts.db >dist\unitTracker\runUnitTracker.bat
 echo %%~dp0\unitTracker.exe demo.db >dist\unitTracker\runDemoAccount.bat
