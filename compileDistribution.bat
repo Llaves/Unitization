24f38d456ad64835a2810397b548bac5d1fbdfc3
Llaves <GitHub@stripfamily.net>
2026-01-12 21:57:00 -0700

    add empty backup directory to zip

--- compileDistribution.bat
+++ compileDistribution.bat
@@ -4,6 +4,7 @@ pyinstaller --onedir unitTracker.py
 copy empty.db dist\unitTracker\accounts.db
 copy empty.db dist\unitTracker\demo.db
 copy icon.ico dist\unitTracker\icon.ico
+mkdir dist\unitTracker\backup
 echo %%~dp0\unitTracker.exe accounts.db >dist\unitTracker\runUnitTracker.bat
 echo %%~dp0\unitTracker.exe demo.db >dist\unitTracker\runDemoAccount.bat
 call .venv\scripts\deactivate.bat
